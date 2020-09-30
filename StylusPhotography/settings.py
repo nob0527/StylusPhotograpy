54714a26dcedaeaee66121705e73df8842203377
Balog Norbert <norbet0527.gmail.com>
2020-09-30 12:26:29 +0200

    page templates

--- StylusPhotography/settings.py
+++ StylusPhotography/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / 'static']
